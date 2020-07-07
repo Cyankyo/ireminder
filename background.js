a99c25dcf80d8a9cbc1bd1259b172a87ec252125
Cyankyo <cyankyo@163.com>
2020-07-07 15:50:05 +0800

    修改通知效果，可以添加10个提醒

--- background.js
+++ background.js
@@ -5,47 +5,106 @@
 'use strict';
 let alarmSet = new Set();
 
-function setReminder(interval, tip) {
-  // interval 单位分钟
-  // tip 提示的消息
-  var st = new Date().getTime() + 60 * 1000 * interval;
-  chrome.alarms.clearAll();
-  chrome.alarms.create("ireminder", {when: st, periodInMinutes:interval});
-  chrome.alarms.onAlarm.addListener(alarm => {
-    if(alarm.name != "ireminder"){
-      return
-    }
-    if(alarmSet.has(alarm.scheduledTime)){
-      return
-    }
+function remove(mouseEvent, alarmTable) {
+    var rowIndex = mouseEvent.srcElement.parentElement.parentElement.rowIndex;
+    var line = mouseEvent.srcElement.parentElement.parentElement;
+    console.log("rowIndex:{}", rowIndex);
+    console.log("line:", line);
+    console.log("alarmTable:", alarmTable);
+    alarmTable.removeChild(line);
 
-    chrome.storage.sync.get(['tip'], result => {
-      if(result.tip){
-        chrome.notifications.create( 
-          {type:"basic", 
-          iconUrl:"images/get_started16.png", 
-          title:"ireminder message",
-          message:result.tip,
-          requireInteraction:true}
-        );
-      }
+    var alarmName = line.children[1].textContent;
+    chrome.alarms.clear(alarmName, function (wasCleared) {
+        if (wasCleared) {
+            // createNotification("删除成功");
+        }
     });
+}
 
-    alarmSet.clear();
-    alarmSet.add(alarm.scheduledTime);
-    console.log("*******Got an alarm!*********", alarm);
-  });
+function setReminder(id, interval, tip) {
+    console.log("id:" + id + ";value:", interval + ";tip:" + tip);
+    if (isNull(interval) || isNull(tip) || isNull(id)) {
+        createNotification("时间或提示不能为空ヽ(ー_ー)ノ");
+        return;
+    }
+    var whenTime = null;
+    var periodInMinutes = null;
+    switch (id) {
+        case "cycleTime":
+            console.log("id~~" + id);
+            whenTime = new Date().getTime() + 60 * 1000 * interval;
+            periodInMinutes = interval * 1;
+            break;
+        case "everyDay":
+            whenTime = new Date(new Date().toLocaleDateString() + " " + interval).getTime();
+            periodInMinutes = 24 * 60;
+            console.log("id~~" + id);
+            break;
+        case "specificTime":
+            console.log("id~~" + id);
+            whenTime = new Date(interval).getTime();
+            break;
+        case "delayTime":
+            console.log("id~~" + id);
+            whenTime = new Date().getTime() + 60 * 1000 * interval;
+            break;
+        default:
+            createNotification("时间设置错误ヽ(ー_ー)ノ");
+            console.log("error");
+            return;
+    }
+    console.log("whenTime:" + whenTime + " periodInMinutes:" + periodInMinutes);
 
-  chrome.storage.sync.set({"interval": interval, "tip": tip});
-  alert("set reminder success");
+    chrome.alarms.create(tip, {when: whenTime, periodInMinutes: periodInMinutes});
+    
 };
 
-function removeReminder() {
-  chrome.alarms.clearAll();
-  alert("remove reminder success");
+function removeAllReminder() {
+    chrome.alarms.clearAll();
+    console.log("remove all reminder success")
+    createNotification("remove all reminder success")
+};
+
+function removeReminder(name) {
+    console.log("clear alarm:{}" + name);
+    chrome.alarms.clear(name, wascleared => {
+        console.log("success?" + wascleared);
+    });
 };
 
 // 一加载插件，就默认设置
-chrome.runtime.onInstalled.addListener(function(reason){
-  setReminder(1, "休息一下眼睛吧!");
+chrome.runtime.onInstalled.addListener(function (reason) {
+    chrome.alarms.onAlarm.addListener(alarm => {
+        chrome.notifications.create(
+            {
+                type: "basic",
+                iconUrl: "images/get_started16.png",
+                title: alarm.name,
+                message: "",
+                requireInteraction: true
+            }
+        );
+        console.log("*******Got an alarm!*********", alarm);
+    });
 });
+
+
+function isNull(str) {
+    if (!str || str === '') return true;
+    var regu = "^[ ]+$";
+    var re = new RegExp(regu);
+    return re.test(str);
+}
+
+function createNotification(msg) {
+
+    chrome.notifications.create(
+        {
+            type: "basic",
+            iconUrl: "images/get_started16.png",
+            title: msg,
+            message: "",
+            requireInteraction: true
+        }
+    );
+}
--- popup.js
+++ popup.js
@@ -1,27 +1,131 @@
 'use strict';
 
 let setRemindButton = document.getElementById('setRemindButton');
-let removeRemindButton = document.getElementById('removeRemindButton');
-
-
-chrome.storage.sync.get(['interval', 'tip'], result => {
-  let intervalInput = document.getElementById('interval');
-  let tipInput = document.getElementById('tip');
-  if(result.interval){
-    intervalInput.value = result.interval;
-    tipInput.value = result.tip;
-    console.info("change the popup");
-  }
+let allDeleteBtn = document.getElementById('allDeleteBtn');
+let allSelectCheckBox = document.getElementById('allSelectCheckBox');
+let alarmTable = document.getElementById("alarmTable");
+chrome.alarms.getAll(alarmsArray => {
+    console.log(alarmsArray);
+    alarmsArray.forEach((item, index, array) => {
+        //增加alarmTable显示通知列表
+        addAlarmLine(item);
+    });
 });
 
-setRemindButton.onclick = function(element) {
-    let interval = document.getElementById('interval');
+setRemindButton.onclick = function () {
+    var timeInput = inputChecked();
     let tip = document.getElementById('tip');
     var bg = chrome.extension.getBackgroundPage();
-    bg.setReminder(parseInt(interval.value), tip.value);
+    if (isRepeat(tip.value)) {
+        bg.createNotification("通知重复啦~~~");
+        return;
+    }
+    if (isOverLimit()) {
+        bg.createNotification("通知超过10个了~~~");
+        return;
+    }
+    bg.setReminder(timeInput.id, timeInput.value, tip.value);
+    chrome.alarms.get(tip.value, function (alarm) {
+        addAlarmLine(alarm);
+    });
 };
 
-removeRemindButton.onclick = function(element) {
-  var bg = chrome.extension.getBackgroundPage();
-  bg.removeReminder();
+function inputChecked() {
+    var inputSelect = document.getElementsByName('interval');
+
+    for (var i = 0, len = inputSelect.length; i < len; i++) {
+        if (inputSelect[i].checked && inputSelect[i].type === 'radio') {
+            return inputSelect[i].parentElement.children[2].children[0];
+        }
+
+    }
+    return null;
+}
+
+allDeleteBtn.onclick = function () {
+    var bg = chrome.extension.getBackgroundPage();
+    var inputSelect = document.getElementsByName('checkboxCell');
+    if (inputSelect.length <= 0) {
+        return;
+    }
+    //获取被选中的alarm name
+    for (var i = inputSelect.length - 1; i >= 0; i--) {
+        console.log("i:" + i);
+        console.log("inputSelect.length:" + inputSelect.length);
+        console.log("inputSelect cell:" + inputSelect[i]);
+        if (inputSelect[i].checked && inputSelect[i].type === 'checkbox') {
+            var line = inputSelect[i].parentElement.parentElement;
+            var nameCell = line.children[1];
+            console.log("remove namecell:" + nameCell.textContent);
+            bg.removeReminder(nameCell.textContent);
+            alarmTable.removeChild(line);
+        }
+    }
+    allSelectCheckBox.checked = false;
+}
+
+allSelectCheckBox.onclick = function () {
+    var inputSelect = document.getElementsByName('checkboxCell');
+    if (inputSelect.length <= 0) {
+        return;
+    }
+    for (var i = 0, len = inputSelect.length; i < len; i++) {
+        inputSelect[i].checked = allSelectCheckBox.checked;
+    }
+}
+
+//是否重名
+function isRepeat(name) {
+    var nameCells = document.getElementsByClassName("alarmName");
+    for (var i = 0; i < nameCells.length; i++) {
+        if (nameCells[i].textContent === name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//是否超出限制
+function isOverLimit() {
+    var nameCells = document.getElementsByClassName("alarmName");
+    return nameCells.length >= 10;
+}
+
+function addAlarmLine(alarm) {
+    var row = document.createElement("tr");
+    var checkCell = document.createElement("td");
+    var check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("name", "checkboxCell");
+    checkCell.appendChild(check);
+    row.appendChild(checkCell);
+
+
+    var nameCell = document.createElement("td");
+    nameCell.textContent = alarm.name;
+    nameCell.setAttribute("class", "alarmName");
+    row.appendChild(nameCell);
+
+    var scheduleCell = document.createElement("td");
+    var dateTime = new Date(alarm.scheduledTime).toLocaleString();
+    scheduleCell.textContent = dateTime.toLocaleString();
+    scheduleCell.setAttribute("class", "alarmTime");
+    row.appendChild(scheduleCell);
+
+
+    var delBtn = document.createElement("input");
+    delBtn.setAttribute("class", "next-btn next-medium next-btn-primary next-btn-warning")
+    delBtn.setAttribute("type", "button");
+    delBtn.value = "删除";
+    delBtn.onclick = function (rowIndex) {
+        var bg = chrome.extension.getBackgroundPage();
+        console.log("rowIndex:" + rowIndex);
+        console.log("rowIndex:" + rowIndex.button);
+        bg.remove(rowIndex, alarmTable);
+    };
+    var delBtnCell = document.createElement("td");
+    delBtnCell.appendChild(delBtn);
+    row.appendChild(delBtnCell);
+
+    alarmTable.appendChild(row);
 }
